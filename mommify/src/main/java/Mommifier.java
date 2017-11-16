a12e445f93fd0bc325c9f71796eb1d886eb52911
Ying Zhou <ybzhou@thoughtworks.com>
2017-11-16 16:56:11 +0800

    finish mommify

--- mommify/src/main/java/Mommifier.java
+++ mommify/src/main/java/Mommifier.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Mommifier {
+    private static final List<String> VOWELS = asList("a", "e", "i", "o", "u");
+
     public String mommify(final String input) {
-        return null;
+        if (null == input) {
+            return null;
+        } else if (Arrays.stream(input.split("")).filter(VOWELS::contains).count() > input.length() / 3.0d) {
+            return input.replaceAll("[aeiou]+", "mommy");
+        } else {
+            return input;
+        }
     }
 }
